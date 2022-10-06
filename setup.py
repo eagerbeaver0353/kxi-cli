67e6b26de43d2118317597872c2503fe8c168efd
Eager <eagerbeaver0353@gmail.com>
2022-10-06 09:32:45 +0100

    Bump version to 1.3.0-rc.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='kxicli',
-    version='1.3.0-rc.6',
+    version='1.3.0-rc.7',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
