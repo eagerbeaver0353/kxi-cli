47572abb68f660bd94fdc3bc5d77600e79495a9c
Eager <eagerbeaver0353@gmail.com>
2023-06-29 08:19:14 +0000

    KXI-28582: Fix profile handling and add tests

--- tests/test_profile_awareness.py
+++ tests/test_profile_awareness.py
@@ -21,15 +21,92 @@ def test_microservices_commands_throw_exception_with_microservices_profile():
     for cmd in commands:
         run_command_with_profile(cmd, DeploymentType.ENTERPRISE)
         
+def test_non_existent_command_throws_exception():
+    commands = ["UNKNOWNCOMMAND"]
+    for cmd in commands:
+        run_command_with_profile(cmd, DeploymentType.ENTERPRISE)
+        run_command_with_profile(cmd, DeploymentType.MICROSERVICES)        
         
-
+        
 def run_command_with_profile(cmd_name, deployment_type):
     if deployment_type == DeploymentType.MICROSERVICES:
         kxicli.main.cli_group.config.config_file = str(Path(__file__).parent / 'files' / 'test-cli-config-microservices')
 
-        result = TEST_CLI.invoke(kxicli.main.cli, [cmd_name])
+    result = TEST_CLI.invoke(kxicli.main.cli, [cmd_name])
 
-        assert result.exit_code == 2
-        assert f"Error: No such command '{cmd_name}'." in result.output
+    assert result.exit_code == 2
+    assert f"Error: No such command '{cmd_name}'." in result.output
 
-        kxicli.main.cli_group.config.config_file = str(Path(__file__).parent / 'files' / 'test-cli-config')    
+    kxicli.main.cli_group.config.config_file = str(Path(__file__).parent / 'files' / 'test-cli-config')    
+
+
+def test_default_profile():
+    kxicli.main.cli_group.config.config_file = str(Path(__file__).parent / 'files' / 'test-cli-config-multi')
+
+    result = TEST_CLI.invoke(kxicli.main.cli, ["query"])
+    
+    cfg = kxicli.main.cli_group.config.config
+    assert cfg.default_section == "default"
+    assert cfg.get(cfg.default_section, "usage") == "enterprise"
+    assert cfg.get(cfg.default_section, "hostname") == "https://test.kx.com"
+    assert cfg.get(cfg.default_section, "namespace") == "test"
+    assert cfg.get(cfg.default_section, "client.id") == "client"
+    assert cfg.get(cfg.default_section, "client.secret") == "secret"
+
+    kxicli.main.cli_group.config.config_file = str(Path(__file__).parent / 'files' / 'test-cli-config')
+
+
+def test_non_default_profile_enterprise():
+    kxicli.main.cli_group.config.config_file = str(Path(__file__).parent / 'files' / 'test-cli-config-multi')
+
+    result = TEST_CLI.invoke(kxicli.main.cli, ["--profile", "enterprise_profile", "query"])
+    
+    cfg = kxicli.main.cli_group.config.config
+    assert cfg.default_section == "enterprise_profile"    
+    assert cfg.get(cfg.default_section, "usage") == "enterprise"
+    assert cfg.get(cfg.default_section, "hostname") == "https://test2.kx.com"
+    assert cfg.get(cfg.default_section, "namespace") == "test2"
+    assert cfg.get(cfg.default_section, "client.id") == "client2"
+    assert cfg.get(cfg.default_section, "client.secret") == "secret2"
+    
+def test_non_default_profile_enterprise_no_default():
+    kxicli.main.cli_group.config.config_file = str(Path(__file__).parent / 'files' / 'test-cli-config-multi-no-default')
+
+    result = TEST_CLI.invoke(kxicli.main.cli, ["--profile", "enterprise_profile", "query"])
+    
+    cfg = kxicli.main.cli_group.config.config
+    assert cfg.default_section == "enterprise_profile"        
+    assert cfg.get(cfg.default_section, "usage") == "enterprise"
+    assert cfg.get(cfg.default_section, "hostname") == "https://test2.kx.com"
+    assert cfg.get(cfg.default_section, "namespace") == "test2"
+    assert cfg.get(cfg.default_section, "client.id") == "client2"
+    assert cfg.get(cfg.default_section, "client.secret") == "secret2"          
+
+    kxicli.main.cli_group.config.config_file = str(Path(__file__).parent / 'files' / 'test-cli-config')
+
+
+def test_non_default_profile_microservices():
+    kxicli.main.cli_group.config.config_file = str(Path(__file__).parent / 'files' / 'test-cli-config-multi')
+
+    result = TEST_CLI.invoke(kxicli.main.cli, ["--profile", "microservices_profile", "query"])
+    
+    cfg = kxicli.main.cli_group.config.config
+    assert cfg.default_section == "microservices_profile"
+    assert cfg.get(cfg.default_section, "usage") == "microservices"
+    assert cfg.get(cfg.default_section, "hostname") == "https://test3.kx.com"
+    assert cfg.get(cfg.default_section, "tp_port") == "5010"
+
+    kxicli.main.cli_group.config.config_file = str(Path(__file__).parent / 'files' / 'test-cli-config')
+
+def test_non_default_profile_microservices_no_default():
+    kxicli.main.cli_group.config.config_file = str(Path(__file__).parent / 'files' / 'test-cli-config-multi-no-default')
+
+    result = TEST_CLI.invoke(kxicli.main.cli, ["--profile", "microservices_profile", "query"])
+    
+    cfg = kxicli.main.cli_group.config.config
+    assert cfg.default_section == "microservices_profile"
+    assert cfg.get(cfg.default_section, "usage") == "microservices"
+    assert cfg.get(cfg.default_section, "hostname") == "https://test3.kx.com"
+    assert cfg.get(cfg.default_section, "tp_port") == "5010"
+
+    kxicli.main.cli_group.config.config_file = str(Path(__file__).parent / 'files' / 'test-cli-config')
